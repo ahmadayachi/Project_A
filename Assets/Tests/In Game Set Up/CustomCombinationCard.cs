375f0fd8600c71403e316a1099be402ded33ae37
Ahmed <ahmed.ayachi1998@outlook.com>
2024-06-01 15:05:08 +0100

    update working on Custom Deck Panel

--- Assets/Tests/In Game Set Up/CustomCombinationCard.cs
+++ Assets/Tests/In Game Set Up/CustomCombinationCard.cs
@@ -1,8 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CustomCombinationCard : MonoBehaviour
 {
     public byte CardRank;
+    public Button CardButton;
+    public Image CardImage;
 }
--- Assets/Tests/In Game Set Up/EpicInGameLogicInitiater.cs
+++ Assets/Tests/In Game Set Up/EpicInGameLogicInitiater.cs
@@ -15,18 +15,27 @@ public class EpicInGameLogicInitiater : MonoBehaviour
     [SerializeField] private Button _onlineMode;
     private InGameLogicModes _mode;
     #endregion
-    #region First Panel Refs 
-    [Header("Game Logic Mod Panel Refs")]
+    #region GameLogic Panels Refs 
+    [Header("Game Logic Mod Panel First Phase Refs")]
     [SerializeField] private GameObject _gameLogicModPanel;
     [SerializeField] private Button _playerNumber;
     [SerializeField] private TextMeshProUGUI _playerNumberText;
-    [SerializeField] private TMP_Dropdown _deckDropThese;
-    [SerializeField] private Button _totalSuitsNumber;
+    [SerializeField] private Toggle _belote;
+    [SerializeField] private Toggle _standart;
+    [SerializeField] private Toggle _custom;
+    [SerializeField] private Button _next;
+    private DeckType _selectedDeckType;
+    [Header("Game Logic Mod Panel Second Phase Refs")]
+    [SerializeField] private GameObject _gameLogicModPanelSecondPhaze;
     [SerializeField] private TextMeshProUGUI _totalSuitsNumberText;
+    [SerializeField] private Button _totalSuitNumber;
     [SerializeField] private Slider _maxCardsInHand;
     [SerializeField] private TextMeshProUGUI _mixMaxCardsInHandsText;
     [SerializeField] private TextMeshProUGUI _maxCardsInHandText;
-    [SerializeField] private Button _next;
+    [SerializeField] private Button _nextTwo;
+    private const int MinCardsInHand = 2;
+    private int _mixMaxCardsInHand;
+    private int _maxCardsInHands;
     #endregion
     #region Custom Deck Panel Refs
     [Header("Custom Deck Mod Panel Refs")]
@@ -34,6 +43,11 @@ public class EpicInGameLogicInitiater : MonoBehaviour
     [SerializeField] private Transform _scrollCardsHolder;
     [SerializeField] private Transform _finalCardsCombinationHolder;
     [SerializeField] private Button _confirm;
+    [SerializeField] private CustomCombinationCard _CustomDeckCardPrefab;
+    List<CustomCombinationCard> _customCombinationCards = new List<CustomCombinationCard>();
+    List<byte> _customDeckCards = new List<byte>();
+    List<byte> _selectedCustomDeckCards = new List<byte>();
+    CardSuit _customDeckSuit = CardSuit.Spades;
     #endregion
     #region Players Info Panel
     [Header("Players Panel Refs")]
@@ -48,6 +62,10 @@ public class EpicInGameLogicInitiater : MonoBehaviour
     private void Awake()
     {
         InitFirstPanelButtons();
+
+        InitGameLogicFirstPhaze();
+
+        InitGameLogicSecondPhaze();
     }
 
 
@@ -90,11 +108,145 @@ public class EpicInGameLogicInitiater : MonoBehaviour
         _multiPeerMode.onClick.AddListener(MultiPeerButton);
     }
     #endregion
-    #region Game Logic Panel Methods
-    private int NumberBumber(string numberText)
+
+    #region Game Logic Panel First phaze Methods
+    private string NumberBumber(string numberText)
     {
         int number = int.Parse(numberText);
-        return number == 8? 1 : ++number;
+        return (number == 8? 1 : ++number).ToString();
+    }
+    private void PlayerNumber()
+    {
+        _playerNumberText.text = NumberBumber(_playerNumberText.text);
+    }
+    private void Belote()
+    {
+        if (_belote.isOn)
+        {
+            _selectedDeckType = DeckType.Belote;
+            _standart.SetIsOnWithoutNotify(false);
+            _custom.SetIsOnWithoutNotify(false);
+        }
+    }
+    private void Standart()
+    {
+        if (_standart.isOn)
+        {
+
+            _selectedDeckType = DeckType.Standard;
+            _belote.SetIsOnWithoutNotify(false);
+            _custom.SetIsOnWithoutNotify(false);
+        }
+    }
+    private void Custom()
+    {
+        if (_custom.isOn)
+        {
+            _selectedDeckType = DeckType.Custom;
+            _belote.SetIsOnWithoutNotify(false);
+            _standart.SetIsOnWithoutNotify(false);
+        }
+    }
+    private void GameLogicNextFirstphaze()
+    {
+        _gameLogicModPanel.SetActive(false);
+
+        if (_selectedDeckType == DeckType.Custom)
+            _CustomDeckModPanel.SetActive(true);
+        else
+            _gameLogicModPanelSecondPhaze.SetActive(true);
+    }
+    private void InitGameLogicFirstPhaze()
+    {
+        _playerNumberText.text = 2.ToString();
+        _playerNumber.onClick.RemoveAllListeners();
+        _playerNumber.onClick.AddListener(PlayerNumber);
+
+        _selectedDeckType = DeckType.Belote;
+        _belote.isOn = true;
+        _standart.isOn = false;
+        _custom.isOn = false;
+
+        _next.onClick.RemoveAllListeners();
+        _next.onClick.AddListener(GameLogicNextFirstphaze);
+    }
+    #endregion
+
+    #region  Game Logic Panel Second phaze Methods
+    private void CalculateMaxCardsInHand()
+    {
+        int playersNumber =int.Parse(_playerNumberText.text);
+        int totalSuitsNumber = int.Parse(_totalSuitsNumberText.text);
+        int CardsInSuitNumber = 0;
+        switch (_selectedDeckType)
+        {
+            case DeckType.Standard: CardsInSuitNumber = 13;break;
+            case DeckType.Belote: CardsInSuitNumber = 8;break;
+            case DeckType.Custom: CardsInSuitNumber = _customDeckCards.Count;break;
+        }
+        int DeckCount = CardsInSuitNumber * totalSuitsNumber;
+        int playerCards = 1;
+        while ((DeckCount - (playerCards * playersNumber) > 0))
+        {
+            playerCards++;
+        }
+        _mixMaxCardsInHand = playerCards - 1;
+        _mixMaxCardsInHandsText.text = _mixMaxCardsInHand.ToString();
+    }
+    private void SuitNumber()
+    {
+        _totalSuitsNumberText.text = NumberBumber(_playerNumberText.text);
+        CalculateMaxCardsInHand();
+    }
+    private void MaxCardsInHandSlider(float value)
+    {
+        _maxCardsInHands =(int)value;
+        _maxCardsInHandText.text = _maxCardsInHand.ToString();
+    }
+    private void SecondPhazeNext()
+    {
+        _gameLogicModPanelSecondPhaze.SetActive(false);
+        _playersPanel.SetActive(true);
+    }
+    private void InitGameLogicSecondPhaze()
+    {
+        _totalSuitsNumberText.text = 2.ToString();
+        CalculateMaxCardsInHand();
+        _totalSuitNumber.onClick.RemoveAllListeners();
+        _totalSuitNumber.onClick.AddListener(SuitNumber);
+        _maxCardsInHand.minValue = MinCardsInHand;
+        _maxCardsInHand.maxValue = _mixMaxCardsInHand;
+        _maxCardsInHand.wholeNumbers = true;
+        _maxCardsInHand.onValueChanged.RemoveAllListeners();
+        _maxCardsInHand.onValueChanged.AddListener(MaxCardsInHandSlider);
+        _nextTwo.onClick.RemoveAllListeners();
+        _nextTwo.onClick.AddListener(SecondPhazeNext);
+    }
+    #endregion
+
+    #region Custom Deck Mod Methods
+    private void InitCustomCombinationCards()
+    {
+        CustomCombinationCard CustomCard;
+
+        _customDeckCards.Clear();
+        _customCombinationCards.Clear();
+        for(int index =1;index<=13;index++)
+        {
+            CustomCard = Instantiate(_CustomDeckCardPrefab);
+            CustomCard.CardRank = (byte)index;
+            CustomCard.CardImage.sprite = AssetLoader.DeckContainerInstance.GetSuitSprite(CustomCard.CardRank, _customDeckSuit);
+            _customCombinationCards.Add(CustomCard);
+            _customDeckCards.Add(CustomCard.CardRank);
+        }
+    }
+    private void OnCustomCardClicked(CustomCombinationCard card)
+    {
+        if(_customDeckCards.Contains(card.CardRank))
+        {
+            _customDeckCards.Remove(card.CardRank);
+
+        }
     }
     #endregion
 }
